31b59830dedc720dcf4fd2882fa511224ee04676
TKende <kendetompos@gmail.com>
2021-11-13 20:01:03 +0100

    done

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,26 @@
+const HU = {
+    date(date) {
+        return (Intl.DateTimeFormat('hu-HU').format(date));
+    },
+    currency(amount) {
+        let huCurrency = Intl.NumberFormat('hu-HU', { style: 'currency', currency: 'HUF' })
+        return huCurrency.format(amount);
+    },
+    list(arr) {
+        let result = "";
+        for (const [i, value] of arr.entries()) {
+            if (i === 0) {
+                result += `${value}`;
+            } else if (i === arr.length - 1) {
+                result += ` és ${value}`;
+            } else {
+                result += `, ${value}`;
+            }
+        }
 
+        return result;
 
+    }
+}
 
-
-
-
-// export default HU
+export default HU
